24aa5edd4fb8112bb7227497c36af0c08282277f
Дмитрий <dzmitry.kovalenok@thehamon.com>
2017-10-07 06:25:56 +0300

    Fix requires in main

--- .main.sh
+++ .main.sh
@@ -1,7 +1,7 @@
 # require this file in .zshrc or .bashrc
 
-source ~/.bash_aliases
-source ~/.bash_helpers
+source ~/.bash_aliases.sh
+source ~/.bash_helpers.sh
 
 [[ -f ~/.current_project ]] && source ~/.current_project
 $CURRENT_PROJECT && cd ~/work/$CURRENT_PROJECT
