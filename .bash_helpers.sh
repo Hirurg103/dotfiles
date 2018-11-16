2b41347c2796f0b100830249aea64930551250f5
Дмитрий <dzmitry.kovalenok@thehamon.com>
2018-11-16 23:54:59 +0300

    Improve bash helpers

--- .bash_helpers.sh
+++ .bash_helpers.sh
@@ -1,14 +1,15 @@
-function goto() {
+function workon() {
   echo "export CURRENT_PROJECT=$1" > ~/.current_project
-  cd "$HOME/work/$1"
+  cd "$HOME/Documents/work/$1"
 }
+alias goto="workon"
 
 function grepgem() {
   ag $2 `bundle show $1`
 }
 
 function grepgems() {
-  ag $1 $(rvm gemdir)"/gems"
+  ag $1 $GEM_HOME"/gems"
 }
 
 function backupit() {
@@ -16,4 +17,22 @@ function backupit() {
   local time=$(date "+%H_%M_%S")
   mkdir -p ~/backups/"$date" >/dev/null 2>&1
   mysqldump -c -e --default-character-set=utf8 --single-transaction --add-drop-database -u$2 $1 | gzip > ~/backups/"$date"/"$2"_dump_"$time".gz
+}
+
+function dump() {
+  time=$(date '+%H_%M_%S');
+  date=$(date '+%Y_%m_%d');
+  database=$1;
+
+  dumps_dir="$HOME/dumps";
+  mkdir -p "$dumps_dir/$date";
+
+  dumpfile=$dumps_dir/$date/$time"_"$database"_dump".psql.gz
+  pg_dump $database -W -U ontheway --no-privileges --clean --exclude-table=schema_migrations | gzip > $dumpfile;
+
+  echo $dumpfile;
+}
+
+function start_android_emulator() {
+  $ANDROID_HOME/emulator/emulator -avd Android_Accelerated_Nougat &>/dev/null &;
 }
